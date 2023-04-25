119f873f1dce7a5a07b88c073716ecf8c49cb29f
alvisevenezia <wantoine@outlook.fr>
2023-04-25 13:30:32 +0200

    inscription php

--- php/inscriptionform.php
+++ php/inscriptionform.php
@@ -6,6 +6,9 @@ $date = $_POST['date'];
 $password = $_POST['password'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
+$adresse = "0";
+$codePostal = "0";
+$ville = "0";
 
 if($_POST["checkHomme"] = "on"){
   $sexe = "Homme";
@@ -19,7 +22,7 @@ $mysqli = new mysqli("127.0.0.1", "root", "", "projetweb");
 
 echo $mysqli->host_info . "\n";
 $result = $mysqli->query("INSERT INTO login(idLogin,mdp) VALUES ('$email','$password')");
-$result = $mysqli->query("INSERT INTO personne(prenom,nom,dateNaissance,codePostal,ville,adresse) VALUES ('$prenom','$nom','$date','0000','0','0')");
+$result = $mysqli->query("INSERT INTO personne(prenom,nom,dateNaissance,codePostal,ville,adresse) VALUES ('$prenom','$nom','$date','$codePostal','$ville','$adresse')");
 
 if(!in_array("sexe",$_COOKIE)){
   setcookie("sexe", "Homme", time() + 3600);
