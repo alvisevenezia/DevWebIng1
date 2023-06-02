536189183a931205e01918cc4f6a55b938f9e258
EliziumAst <louisclementcourtois@gmail.com>
2023-06-02 19:33:14 +0200

    bcp de changements sheeesh

--- navbar.php
+++ navbar.php
@@ -1,21 +1,25 @@
 <nav class="navbar">
     <ul>
-        <div></div>
-        <li <?php if(isset($_GET['sexe']) && $_GET['sexe']=='femme'){
+        <div class="flex navdiv space">
+            <div class="flex navdiv">
+                <li <?php if(isset($_GET['sexe']) && $_GET['sexe']=='femme'){
                         ?> style="border: 1px solid;" <?php } else{
                         ?> class="border" <?php }
                         ?>><a href="index.php?sexe=femme">FEMME</a></li>
 
-        <li <?php if(isset($_GET['sexe']) && $_GET['sexe']=='homme'){
+                <li <?php if(isset($_GET['sexe']) && $_GET['sexe']=='homme'){
             echo"style='border: 1px solid;'"; } 
             else{
             echo"class='border'"; }
             ?>><a href="index.php?sexe=homme">HOMME</a></li>
-
-        <li class="titre">VEPRI</li>
-        <li class="logo">\V</li>
-        <li class="border"><a href="panier.php">Panier</a></li>
-        <?php
+            </div>
+            <div class="flex navdiv">
+                <li class="titre">VEPRI</li>
+                <li class="logo">\V</li>
+            </div>
+            <div class="flex navdiv">
+                <li class="border"><a href="panier.php">Panier</a></li>
+                <?php
 
         if(isset($_SESSION['logged']) && $_SESSION["logged"] == "true"){
             echo "<li class='border'><a href='./php/deconnexion.php'>Deconnexion</a></li>";
@@ -32,5 +36,7 @@
             echo "<li class='border'><a href='./connexion.php'>Connexion</a></li>";
             }
         ?>
+            </div>
+        </div>
     </ul>
 </nav>
